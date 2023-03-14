1fc5ac61d49358f5fa97a840ce08d5dc76b19f11
retrogamer234 <101521533+retrogamer234@users.noreply.github.com>
2023-03-15 01:41:54 +0530

    Update boobjob.js

--- Organs/commands/Nsfw/boobjob.js
+++ Organs/commands/Nsfw/boobjob.js
@@ -21,7 +21,7 @@ module.exports={
     let buttonMessage = {
         image: {url:ass},
         caption: `*😈*`,
-        footer: `*©Eternity-Team*`,
+        footer: `*ᴍɪᴛꜱᴜ*`,
         buttons: buttons,
         headerType: 4
     }
