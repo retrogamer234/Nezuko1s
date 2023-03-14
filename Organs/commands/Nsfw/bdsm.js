fd8b8c6211c09c4f402c1733813deb62bfa8bfe3
retrogamer234 <101521533+retrogamer234@users.noreply.github.com>
2023-03-15 01:41:21 +0530

    Update bdsm.js

--- Organs/commands/Nsfw/bdsm.js
+++ Organs/commands/Nsfw/bdsm.js
@@ -21,7 +21,7 @@ module.exports={
     let buttonMessage = {
         image: {url:ass},
         caption: `*⛓️*`,
-        footer: `*©Eternity-Team*`,
+        footer: `*ᴍɪᴛꜱᴜ*`,
         buttons: buttons,
         headerType: 4
     }
