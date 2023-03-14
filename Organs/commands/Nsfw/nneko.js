cf4703f6de452efbfe9a124836127387ec00abd3
retrogamer234 <101521533+retrogamer234@users.noreply.github.com>
2023-03-15 01:42:51 +0530

    Update nneko.js

--- Organs/commands/Nsfw/nneko.js
+++ Organs/commands/Nsfw/nneko.js
@@ -25,7 +25,7 @@ module.exports = {
     let buttonMessage = {
       image: { url: wife.data.url },
       caption: `*🥵 NYAAAAAAA*`,
-      footer: `*©Eternity-Team*`,
+      footer: `*ᴍɪᴛꜱᴜ*`,
       buttons: buttons,
       headerType: 4,
     };
