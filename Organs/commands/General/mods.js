dd94bd4a83751c8a0321c50afa4d6c5aee12822a
retrogamer234 <101521533+retrogamer234@users.noreply.github.com>
2023-03-15 01:29:26 +0530

    Update mods.js

--- Organs/commands/General/mods.js
+++ Organs/commands/General/mods.js
@@ -11,7 +11,7 @@ module.exports = {
     const mod = global.owner;
     const owner = mod.map((mod) => mod + "@s.whatsapp.net");
     let mo = `
-*🎉 Nᴇᴢᴜᴋᴏ Mᴏᴅs 🎉*\n`;
+*🎉 ɴᴇᴢᴜᴋᴏ ᴏᴡɴᴇʀ 🎉*\n`;
     for (let i = 0; i < mod.length; i++) {
       const um = await client.username(mod[i] + "@s.whatsapp.net");
       mo += `\n*👑 Name:* ${um}\n*🚀 Contact:* @${mod[i].split("@")[0]}\n`;
