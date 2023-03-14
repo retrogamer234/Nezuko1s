64c06ea095997cc1ce3850891d621d010a73523c
retrogamer234 <101521533+retrogamer234@users.noreply.github.com>
2023-03-15 01:43:09 +0530

    Update nwaifu.js

--- Organs/commands/Nsfw/nwaifu.js
+++ Organs/commands/Nsfw/nwaifu.js
@@ -21,7 +21,7 @@ module.exports={
     let buttonMessage = {
         image: {url:wife.data.url},
         caption: `*🥵 AHHHH*`,
-        footer: `*©Eternity-Team*`,
+        footer: `*ᴍɪᴛꜱᴜ*`,
         buttons: buttons,
         headerType: 4
     }
